b2ea488d5c4d970f618a9fbbcf7f45c20a9b7e54
Cristian <Cristian@MacBook-Pro-de-Cristian.local>
2014-08-26 21:36:11 -0500

    Propuesta visual

---
+++ ServicioEmpleo/ServicioEmpleo/Controllers/EmpleoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicioEmpleo.Controllers
+{
+    public class EmpleoController : Controller
+    {
+        //
+        // GET: /Empleo/
+
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Publicar()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+
+    }
+}
